232c91b327e0f2a394918583d21bbf0feb0cb239
jcreixell <jcreixell@gmail.com>
2015-08-17 00:51:18 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,26 @@
+## Put.io Sync
+
+Simple script to sync files between put.io downloads and local storage. It uses a small database to keep track of already downloaded files, so that the same file is never downloaded twice (even if it's deleted or moved).
+
+## Requirements
+
+Sqlite
+Python 2.x (see python required libraries in requirements.txt)
+
+## Getting Strted
+
+Create a sqlite database:
+
+```
+sqlite3 putio.db
+
+> CREATE TABLE downloads (id int, name string);
+> .q
+```
+
+Create a file config.yml with your settings (see config.yml.sample)
+
+## License
+
+putio-sync is released under the [MIT License](http://www.opensource.org/licenses/MIT).
+
---
+++ putio_sync.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sqlite3
+import putio
+import datetime
+import yaml
+
+# Load config
+config_file = open('config.yml', 'r')
+config = yaml.load(config_file.read())
+config_file.close()
+
+
+print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " Starting..."
+conn = sqlite3.connect(config['db']['path'])
+c = conn.cursor()
+c.execute("SELECT id FROM downloads")
+existing =  [i[0] for i in c.fetchall()]
+
+helper = putio.AuthHelper(config['putio']['client_id'], config['putio']['client_secret'], config['putio']['callback_url'], type='token')
+client = putio.Client(config['putio']['oauth_token'])
+files = client.File.list()
+
+for file in files:
+	if not file.id in existing:
+		print "Downloading " + file.name + "..."
+		file.download(config['download']['path'])
+		c.execute("INSERT INTO downloads VALUES (?,?)", (file.id, file.name))
+		conn.commit()
+		print "Done."
+
+conn.close()
+print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " done."
